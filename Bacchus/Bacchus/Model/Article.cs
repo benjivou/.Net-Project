0cb3c7c153fbfceafb63f71cecdd7ab3dcc838a4
YohanDerenne <yohan.derenne@gmail.com>
2020-03-31 11:09:24 +0200

    add all model classes

---
+++ Bacchus/Bacchus/Model/Article.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bacchus.Model
+{
+    class Article
+    {
+        public string RefArticle { get; set; }
+        public string Description { get; set; }
+        public SousFamille SousFamille { get; set; }
+        public Marque Marque { get; set; }
+        public float PrixHT { get; set; }
+        public int Quantite { get; set; }
+
+        public Article(string Ref, string Descrip, float Price, int Quantity, Marque Brand, SousFamille ChildFamily)
+        {
+            RefArticle = Ref;
+            Description = Descrip;
+            PrixHT = Price;
+            Quantite = Quantity;
+            Marque = Brand;
+            SousFamille = ChildFamily;
+        }
+    }
+}
